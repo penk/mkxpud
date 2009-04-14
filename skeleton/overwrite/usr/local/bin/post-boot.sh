2121d5bf781a28d389da824351df268d3e32958d
Chen, Ping-Hsun <penkia@gmail.com>
2009-04-14 20:29:21 +0800

    add es and de plate locale add german keymap add netdaemon to post-boot and recipe add get_flash script add kmap= cheatcode add ra0 to NIC list

--- skeleton/overwrite/usr/local/bin/post-boot.sh
+++ skeleton/overwrite/usr/local/bin/post-boot.sh
@@ -1,19 +1,35 @@
+#!/bin/bash
+
 if [ ! -e /tmp/firsttime ]; then
 
+# run this script once
 touch /tmp/firsttime
 
+# start udev daemon
 udevd --daemon
 udevadm trigger
 
-for NIC in eth0 eth1 eth2 wlan0 ath0; do
+# work through NIC 
+for NIC in eth0 eth1 eth2 wlan0 ath0 ra0; do
 	/bin/ifconfig $NIC up
 	/sbin/iwconfig $NIC mode Managed
 done
+/usr/local/bin/netdaemon eth0
 
+# setup sound channel
 for channel in Master Front; do
 	/usr/bin/amixer set $channel 80%
 done
 
+# keymap
+if [ ! -z $KEYMAP ] && [ -e /usr/local/share/keymap/$KEYMAP.kmap ]; then 
+	loadkmap < /usr/local/share/keymap/$KEYMAP.kmap
+fi
+
+# execute customize script 
+[ -e /sbin/customize.sh ] && . /sbin/customize.sh 
+
+# start hotplug script
 /bin/cp /sbin/hotplug-x /sbin/hotplug
 
 fi 
---
+++ skeleton/overwrite/usr/share/plate/chrome/content/plate.es.js
@@ -0,0 +1,149 @@
+
+function select_tab( this_tab ) {
+	if( this_tab.className == '' ) {
+		var tabs = document.getElementById('panel').getElementsByTagName('div');
+		for( var i = 0; i < tabs.length; i++ ) {
+			if( tabs[i].className == 'selected' ) {
+				tabs[i].className = '';
+				tabs[i].getElementsByTagName('div')[0].className = '';
+				break;
+			}
+		}
+		this_tab.className = 'selected';
+		this_tab.getElementsByTagName('div')[0].className = 'under_cover';
+		show_menu( this_tab.id );
+	}
+	else if( document.getElementById('menu').className == '' )
+		show_menu( this_tab.id );
+}
+
+function show_menu( input ) {
+	netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect"); 
+	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+	file.initWithPath("/usr/share/plate/chrome/content/template/" + input + ".html" );
+
+	var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
+	var sstream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
+	fstream.init(file, -1, 0, 0);
+	sstream.init(fstream); 
+
+	var data = "";
+	var str = sstream.read(4096);
+	while (str.length > 0) {
+		data += str;
+		str = sstream.read(4096);
+	}
+
+	sstream.close();
+	fstream.close();
+
+	var utf8Converter = Components.classes["@mozilla.org/intl/utf8converterservice;1"].getService(Components.interfaces.nsIUTF8ConverterService);
+    data = utf8Converter.convertURISpecToUTF8 (data, "UTF-8");
+    
+	document.getElementById('close_button').style.display = "none";
+	document.getElementById('programs').className = '';
+	document.getElementById('menu').className = 'show';
+	document.getElementById('menu').innerHTML = data;
+}
+
+function resume_notify( this_obj, program_name ) {
+	this_obj = this_obj.parentNode.getElementsByTagName('div')[0];
+	if( document.getElementById( "exec." + program_name ) != null )
+		this_obj.className = 'notify_resume';
+	else
+		this_obj.className = 'notify_hide';
+}
+
+function show_program( input ) {
+	document.getElementById('close_button').style.display = "inline";
+	document.getElementById('menu').className = '';
+	document.getElementById('programs').className = 'show';
+	if( document.getElementById("exec." + input) == null ) {
+		var new_element = document.createElement("div");
+		new_element.id = "exec."+input;
+		new_element.innerHTML = "<embed src=chrome://plate/content/utils/"+input+" width=100% height=100% />";
+		document.getElementById('programs').appendChild(new_element);
+	}
+	var programs = document.getElementById('programs').getElementsByTagName('div');
+	for( var i = 0; i < programs.length; i++ )
+		if( programs[i].className == 'show' ) {
+			programs[i].className = '';
+			break;
+		}
+	document.getElementById("exec."+input).className = 'show';
+}
+
+function close_program() {
+	var programs = document.getElementById('programs').getElementsByTagName('div');
+	for( var i = 0; i < programs.length; i++ )
+		if( programs[i].className == 'show' )
+			document.getElementById('programs').removeChild(programs[i]);
+	var tabs = document.getElementById('panel').getElementsByTagName('div');
+	for( var i = 0; i < tabs.length; i++ )
+		if( tabs[i].className == "selected" )
+			show_menu( tabs[i].id );
+}
+
+function $()
+{
+  var elements = new Array();
+  for (var i = 0; i < arguments.length; i++)
+  {
+    var element = arguments[i];
+    if (typeof element == 'string')
+      element = document.getElementById(element);
+    if (arguments.length == 1)
+      return element;
+    elements.push(element);
+  }
+  return elements;
+}
+
+function system(input) {
+	netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect"); 
+	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+	file.initWithPath("/usr/local/bin/jswrapper");
+	var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
+	process.init(file);
+	var args = [input];
+	process.run(false, args, 1);
+}
+
+function update() {
+
+	sleep(50);
+
+	netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect"); 
+	var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+	file.initWithPath("/tmp/jswrapper.log");
+
+	var data = "";
+	document.getElementById('info').innerHTML = "";
+	var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
+	var sstream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
+	fstream.init(file, -1, 0, 0);
+	sstream.init(fstream); 
+
+	var str = sstream.read(4096);
+	while (str.length > 0) {
+		data += str;
+		str = sstream.read(4096);
+	}
+
+	sstream.close();
+	fstream.close();
+
+	document.getElementById('info').innerHTML = data;
+}
+
+function sleep(milliseconds) {
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+		if ((new Date().getTime() - start) > milliseconds) break;
+	}
+}
+
+function confirm_off() {
+	tmp = window.confirm('Quiere apagar el equipo?'); 
+	if (tmp) system('poweroff -f');
+}
