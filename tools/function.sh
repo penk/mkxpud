a6c1baf66f44eb55cb7b2f355ec5dab80e11d9d3
Chen, Ping-Hsun <penkia@gmail.com>
2009-05-02 03:47:24 +0800

    add du command for each image formats

--- tools/function.sh
+++ tools/function.sh
@@ -196,6 +196,7 @@ function image {
 				cp $MKXPUD_KERNEL_IMAGE deploy/$MKXPUD_CODENAME/iso/boot/bzImage
 				cp deploy/$MKXPUD_CODENAME/rootfs.gz deploy/$MKXPUD_CODENAME/iso/boot/
 				mkisofs -R -l -V 'xPUD' -input-charset utf-8 -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o deploy/$MKXPUD_CODENAME.iso deploy/$MKXPUD_CODENAME/iso/
+				du -h deploy/$MKXPUD_CODENAME.iso
 			;;
 			exe)
 				cp -r skeleton/boot/exe/ deploy/$MKXPUD_CODENAME/
@@ -205,6 +206,7 @@ function image {
 				../../../tools/makensis xpud-installer.nsi
 				cd -
 				cp deploy/$MKXPUD_CODENAME/exe/xpud-installer.exe deploy/$MKXPUD_CODENAME.exe
+				du -h deploy/$MKXPUD_CODENAME.exe
 			;;
 			*)
 			echo "$format: not supported format"
