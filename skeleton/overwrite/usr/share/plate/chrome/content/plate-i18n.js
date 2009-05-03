17ed9dc3f750525c5135c332803de5814df02530
Chen, Ping-Hsun <penkia@gmail.com>
2009-05-03 18:43:54 +0800

    add French translation and more German/Chinese translation

--- skeleton/overwrite/usr/share/plate/chrome/content/plate-i18n.js
+++ skeleton/overwrite/usr/share/plate/chrome/content/plate-i18n.js
@@ -38,6 +38,7 @@ var i18n =  {
     	de: "Willkommen bei xPUD",
     	en: "Welcome to xPUD",
     	es: "Bienvenido a xPUD",
+    	fr: "Bienvenue sur xPUD",
     	zh: "歡迎使用 xPUD",
     },
     
@@ -45,6 +46,7 @@ var i18n =  {
 		de: "Systeminformationen",
 		en: "System Information",
 		es: "Informaci&oacute;n del sistema",
+		fr: "Information du système",
 		zh: "系統資訊",
 	},
 
@@ -52,38 +54,47 @@ var i18n =  {
 		de: "Sie k&ouml;nnen:",
 		en: "You may want to:",
 		es: "Quiz&aacute;s desee:",
+		fr: "Vous voulez:",
 		zh: "你也可以：",
 	},
 	power_off: {
 		de: "Ausschalten",
 		en: "Power Off",
 		es: "Apagar",
+		fr: "Éteindre",
 		zh: "關機",
 	},
 	power_off_desc: {
 		de: "Dieses Ger&auml;t ausschalten",
 		en: "turn off this machine",
 		es: "Apagar su equipo",
+		fr: "éteindre l'ordinateur",
 		zh: "關閉這台電腦",
 	},
 	switch_mode: {
 		de: "System wechseln",
 		en: "Switch Mode",
 		es: "Cambiar SO",
+		fr: "Changer de mode",
 		zh: "切換模式",
 	},
 	switch_mode_desc: {
 		de: "Anderes Betriebssystem starten",
 		en: "toggle into other OS",
 		es: "Cambiar a otro Sistema Operativo",
+		fr: "basculer dans un autre OS",
 		zh: "使用其他作業系統",
 	},
 	get_help: {
 		en: "Get Help",
+		de: "Hilfe",
+		fr: "Obtenir de l'aide",
 		zh: "取得幫助",
 	},
 	get_help_desc: {
 		en: "and contact information",
+		de: "und Kontaktinformationen",
+		fr: "information et contact",
 		zh: "和更多資訊",
 	},
 	
@@ -92,56 +103,172 @@ var i18n =  {
 		de: "Programme",
 		en: "Applications",
 		es: "Aplicaciones",
+		fr: "Applications",
 		zh: "所有應用程式",
 	},
 	
 	web_browser: {
 		en: "Web Browser",
 		es: "Navegador web",
+		fr: "Navigateur Web",
 		zh: "網頁瀏覽器",
 	},
 	bt_client: {
 		en: "BitTorrent Client",
 		es: "Cliente BitTorrent",
+		fr: "Client BitTorrent",
 		zh: "BT 下載",
 	},
 	media_player: {
 		en: "Media Player",
 		es: "Reproductor Multimedia",
+		fr: "Lecteur Multimedia",
 		zh: "影音播放",
 	},
 	picture_viewer: {
 		en: "Picture Viewer",
 		es: "Visor de im&aacute;genes",
+		fr: "Visionneuse d'images",
 		zh: "圖片瀏覽",
 	},
 	text_editor: {
 		en: "Text Editor",
 		es: "Editor de textos",
+		fr: "Éditeur de textes",
 		zh: "文字編輯",
 	},
 	pdf_viewer: {
 		en: "PDF Viewer",
+		fr: "PDF Viewer",
 		zh: "PDF 閱讀",
 	},
 	terminal_emulator: {
 		en: "Terminal Emulator",
 		es: "Emulador de terminal",
+		fr: "Émulateur de terminal",
 		zh: "終端機",
 	},
 	
 // setting
 
-	personal: {
+	setting_personal: {
 		en: "Personal",
 		zh: "個人設定",
 	},
+	
+		setting_language: {
+			en: "Language",
+			de: "Sprache",
+			fr: "Langue",
+			zh: "語言選項",
+		},
+		setting_language_desc: {
+			en: "choose locale and keymap",
+			de: "sprache und tastenbelegung",
+			fr: "choisir la langue et la disposition du clavier",
+			zh: "切換語系和鍵盤",
+		}, 
+	
+		setting_time_date: {
+			en: "Time & Date",
+			de: "Datum und Uhrzeit",
+			fr: "Date et heure",
+			zh: "時間日期",
+		},
+		setting_time_date_desc: {
+			en: "change system clock",
+			de: "systemuhr einstellen",
+			fr: "changer l'heure du système",
+			zh: "調整系統日期與時間",
+		},
+		setting_backup: {
+			en: "Backup",
+			fr: "Sauvegarde",
+			zh: "儲存資料",
+		},
+		setting_backup_desc: {
+			en: "save your data and changes",
+			de: "daten und einstellungen sichern",
+			fr: "sauver vos données et modifications",
+			zh: "保留你的設定和資料",
+		},
+
+
 	setting_system: {
 		en: "System",
+		fr: "Système",
 		zh: "系統設定",
 	},
+	
+		setting_wifi: {
+			en: "WiFi/3G",
+			zh: "WiFi/3G",
+		},
+		setting_wifi_desc: {
+			en: "setup wireless connection",
+			de: "drahtloses netzwerk einrichten",
+			zh: "使用無線網路連線",
+		},
+		setting_ethernet: {
+			en: "Ethernet",
+			zh: "有線網路",
+			
+		},
+		setting_ethernet_desc: {
+			en: "setup wired connection",
+			de: "kabelgebundenes netzwerk einrichten",
+			fr: "configurer la connexion Internet",
+			zh: "連接到網際網路",
+		},
+		setting_opt: {
+			en: "Opt-Get",
+			zh: "Opt-Get",
+		},
+		setting_opt_desc: {
+			en: "get more apps",
+			de: "weitere programme installieren",
+			fr: "obtenir plus d'applications",
+			zh: "安裝更多額外程式",
+		},
+	
 	setting_hardware: {
 		en: "Hardware",
 		zh: "硬體設定",
 	},
+		setting_sound: {
+			en: "Sound",
+			de: "Ton",
+			fr: "Son",
+			zh: "聲音",
+		},
+		setting_sound_desc: {
+			en: "adjust system sound",
+			de: "Systemkl&auml;nge einstellen",
+			fr: "ajuster les son système",
+			zh: "調整系統音量設定",
+		},
+		setting_display: {
+			en: "Display",
+			de: "Bildschirm",
+			fr: "Écran",
+			zh: "螢幕",
+		},
+		setting_display_desc: {
+			en: "change screen resolution",
+			de: "Aufl&ouml;sung einstellen",
+			fr: "changer la résolution de l'écran",
+			zh: "調整螢幕解析度",
+		},
+		setting_driver: {
+			en: "Driver",
+			de: "Treiber",
+			fr: "Pilotes",
+			zh: "裝置管理",
+		},
+		setting_driver_desc: {
+			en: "device info and driver",
+			de: "ger&auml;teinformationen und treiber",
+			fr: "informations et drivers des périphériques",
+			zh: "安裝更多驅動程式",
+		},
 };
