94a759411da85ca019e5d4d9d297f8dba1e2edb1
Chen, Ping-Hsun <penkia@gmail.com>
2009-05-03 19:35:25 +0800

    applied es translation patch from Sergio

--- skeleton/overwrite/usr/share/plate/chrome/content/plate-i18n.js
+++ skeleton/overwrite/usr/share/plate/chrome/content/plate-i18n.js
@@ -5,29 +5,29 @@ var i18n =  {
 // tab 
 
     home : {
-    	de: "Start",
         en: "Home",
+    	de: "Start",
         es: "Inicio",
         zh: "回到主頁",
     },
     
     menu : {
-    	de: "Programme",
     	en: "Menu",
+    	de: "Programme",
     	es: "Men&uacute;",
     	zh: "執行程式",
     },
 
     file : {
-    	de: "Dateien",
     	en: "File",
+    	de: "Dateien",
     	es: "Archivos",
     	zh: "開啟檔案",
     },
 
     setting : {
-    	de: "Einstellung",
     	en: "Setting",
+    	de: "Einstellung",
     	es: "Preferencias",
     	zh: "設定系統",
     },
@@ -35,52 +35,52 @@ var i18n =  {
 // home 
 
     welcome : {
-    	de: "Willkommen bei xPUD",
     	en: "Welcome to xPUD",
+    	de: "Willkommen bei xPUD",
     	es: "Bienvenido a xPUD",
     	fr: "Bienvenue sur xPUD",
     	zh: "歡迎使用 xPUD",
     },
     
 	sys_info: {
-		de: "Systeminformationen",
 		en: "System Information",
+		de: "Systeminformationen",
 		es: "Informaci&oacute;n del sistema",
 		fr: "Information du système",
 		zh: "系統資訊",
 	},
 
 	want_to: {
-		de: "Sie k&ouml;nnen:",
 		en: "You may want to:",
+		de: "Sie k&ouml;nnen:",
 		es: "Quiz&aacute;s desee:",
 		fr: "Vous voulez:",
 		zh: "你也可以：",
 	},
 	power_off: {
-		de: "Ausschalten",
 		en: "Power Off",
+		de: "Ausschalten",
 		es: "Apagar",
 		fr: "Éteindre",
 		zh: "關機",
 	},
 	power_off_desc: {
-		de: "Dieses Ger&auml;t ausschalten",
 		en: "turn off this machine",
+		de: "Dieses Ger&auml;t ausschalten",
 		es: "Apagar su equipo",
 		fr: "éteindre l'ordinateur",
 		zh: "關閉這台電腦",
 	},
 	switch_mode: {
-		de: "System wechseln",
 		en: "Switch Mode",
+		de: "System wechseln",
 		es: "Cambiar SO",
 		fr: "Changer de mode",
 		zh: "切換模式",
 	},
 	switch_mode_desc: {
-		de: "Anderes Betriebssystem starten",
 		en: "toggle into other OS",
+		de: "Anderes Betriebssystem starten",
 		es: "Cambiar a otro Sistema Operativo",
 		fr: "basculer dans un autre OS",
 		zh: "使用其他作業系統",
@@ -88,20 +88,22 @@ var i18n =  {
 	get_help: {
 		en: "Get Help",
 		de: "Hilfe",
+		es: "Obtener Ayuda",
 		fr: "Obtenir de l'aide",
 		zh: "取得幫助",
 	},
 	get_help_desc: {
 		en: "and contact information",
 		de: "und Kontaktinformationen",
+		es: "e informaci&oacute;n de contacto",
 		fr: "information et contact",
 		zh: "和更多資訊",
 	},
 	
 // menu
 	application: {
-		de: "Programme",
 		en: "Applications",
+		de: "Programme",
 		es: "Aplicaciones",
 		fr: "Applications",
 		zh: "所有應用程式",
@@ -140,6 +142,7 @@ var i18n =  {
 	pdf_viewer: {
 		en: "PDF Viewer",
 		fr: "PDF Viewer",
+		es: "Visor de PDF",
 		zh: "PDF 閱讀",
 	},
 	terminal_emulator: {
@@ -153,18 +156,21 @@ var i18n =  {
 
 	setting_personal: {
 		en: "Personal",
+		es: "Personal",
 		zh: "個人設定",
 	},
 	
 		setting_language: {
 			en: "Language",
 			de: "Sprache",
+			es: "Idioma",
 			fr: "Langue",
 			zh: "語言選項",
 		},
 		setting_language_desc: {
 			en: "choose locale and keymap",
 			de: "sprache und tastenbelegung",
+			es: "elegir el idioma y la distribuci&oacute;n del teclado",
 			fr: "choisir la langue et la disposition du clavier",
 			zh: "切換語系和鍵盤",
 		}, 
@@ -173,22 +179,26 @@ var i18n =  {
 			en: "Time & Date",
 			de: "Datum und Uhrzeit",
 			fr: "Date et heure",
+			es: "Fecha y hora",
 			zh: "時間日期",
 		},
 		setting_time_date_desc: {
 			en: "change system clock",
 			de: "systemuhr einstellen",
+			es: "cambiar la hora del sistema",
 			fr: "changer l'heure du système",
 			zh: "調整系統日期與時間",
 		},
 		setting_backup: {
 			en: "Backup",
+			es: "Copia de Seguridad",
 			fr: "Sauvegarde",
 			zh: "儲存資料",
 		},
 		setting_backup_desc: {
 			en: "save your data and changes",
 			de: "daten und einstellungen sichern",
+			es: "salvar sus datos y modificaciones",
 			fr: "sauver vos données et modifications",
 			zh: "保留你的設定和資料",
 		},
@@ -196,78 +206,91 @@ var i18n =  {
 
 	setting_system: {
 		en: "System",
+		es: "Sistema",
 		fr: "Système",
 		zh: "系統設定",
 	},
 	
 		setting_wifi: {
 			en: "WiFi/3G",
+			es: "WiFi/3G",
 			zh: "WiFi/3G",
 		},
 		setting_wifi_desc: {
 			en: "setup wireless connection",
 			de: "drahtloses netzwerk einrichten",
+			es: "configurar la conexi&oacute;n inal&aacute;mbrica",
 			zh: "使用無線網路連線",
 		},
 		setting_ethernet: {
 			en: "Ethernet",
+			es: "Red Cableada",
 			zh: "有線網路",
-			
 		},
 		setting_ethernet_desc: {
 			en: "setup wired connection",
 			de: "kabelgebundenes netzwerk einrichten",
+			es: "configurar la conexi&oacute;n cableada",
 			fr: "configurer la connexion Internet",
 			zh: "連接到網際網路",
 		},
 		setting_opt: {
 			en: "Opt-Get",
+			es: "Opt-Get",
 			zh: "Opt-Get",
 		},
 		setting_opt_desc: {
 			en: "get more apps",
 			de: "weitere programme installieren",
+			es: "obtener m&aacute;s aplicaciones",
 			fr: "obtenir plus d'applications",
 			zh: "安裝更多額外程式",
 		},
 	
 	setting_hardware: {
 		en: "Hardware",
+		es: "Hardware",
 		zh: "硬體設定",
 	},
 		setting_sound: {
 			en: "Sound",
 			de: "Ton",
+			es: "Sonido",
 			fr: "Son",
 			zh: "聲音",
 		},
 		setting_sound_desc: {
 			en: "adjust system sound",
 			de: "Systemkl&auml;nge einstellen",
+			es: "ajustar el sonido del sistema",
 			fr: "ajuster les son système",
 			zh: "調整系統音量設定",
 		},
 		setting_display: {
 			en: "Display",
 			de: "Bildschirm",
+			es: "Pantalla",
 			fr: "Écran",
 			zh: "螢幕",
 		},
 		setting_display_desc: {
 			en: "change screen resolution",
 			de: "Aufl&ouml;sung einstellen",
+			es: "cambiar la resoluci&oacute;n de la pantalla",
 			fr: "changer la résolution de l'écran",
 			zh: "調整螢幕解析度",
 		},
 		setting_driver: {
 			en: "Driver",
 			de: "Treiber",
+			es: "Controladores",
 			fr: "Pilotes",
 			zh: "裝置管理",
 		},
 		setting_driver_desc: {
 			en: "device info and driver",
 			de: "ger&auml;teinformationen und treiber",
+			es: "informaci&oacute;n de los controladores de los perif&eacute;ricos",
 			fr: "informations et drivers des périphériques",
 			zh: "安裝更多驅動程式",
 		},
