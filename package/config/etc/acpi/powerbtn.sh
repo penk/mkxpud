a86cce938e5dd1c10311abc16cbf2c0e20723952
joshas <joshas@gmail.com>
2009-05-20 19:15:59 +0300

    Added acpid and one sample event

---
+++ package/config/etc/acpi/powerbtn.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# /etc/acpi/powerbtn.sh
+# Initiates a shutdown when the power putton has been
+# pressed.
+
+# Skip if we just in the middle of resuming.
+test -f /var/lock/acpisleep && exit 0
+
+# Initiate a plain shutdown.
+/bin/poweroff -f
