0977816bab35160e07f5af0e90b03f8bd6c0cac2
Chen, Ping-Hsun <penkia@gmail.com>
2009-05-26 03:55:25 +0800

    fix file path for upx add missing checking

--- tools/function.sh
+++ tools/function.sh
@@ -236,7 +236,7 @@ function post {
 	./tools/busybox-helper
 
 	# check dependencies of each files under usr/lib
-	for s in `find $MKXPUD_TARGET/usr/lib/*.so.* $MKXPUD_TARGET/usr/share/firefox/components/*.so`; do 
+	for s in `find $MKXPUD_TARGET/usr/lib/*.so.* $MKXPUD_TARGET/usr/share/firefox/components/*.so $MKXPUD_TARGET/usr/share/firefox/*.so`; do 
 	
 		if [ ! -d $s ]; then 
 		for i in `./tools/ldd-helper $s`; do 
