0e2e931bfdaefab6fc2a68750d2e264a3ea2d73e
joshas <joshas@gmail.com>
2009-08-06 13:30:04 +0300

    Cleaned up Firefox settings, added useful default bookmarks.

--- package/config/.mozilla/firefox/ofozxweu.default/prefs.js
+++ package/config/.mozilla/firefox/ofozxweu.default/prefs.js
@@ -15,19 +15,34 @@ user_pref("app.update.lastUpdateTime.blocklist-background-update-timer", 1012992
 user_pref("app.update.lastUpdateTime.microsummary-generator-update-timer", 1012992265);
 user_pref("app.update.lastUpdateTime.places-maintenance-timer", 1012992282);
 user_pref("app.update.lastUpdateTime.search-engine-update-timer", 1012992267);
+//user_pref("browser.bookmarks.restore_default_bookmarks", false);
 user_pref("browser.download.manager.showWhenStarting", false);
 user_pref("browser.link.open_newwindow.restriction", 0);
 user_pref("browser.migration.version", 1);
 user_pref("browser.places.importBookmarksHTML", false);
+//user_pref("browser.places.smartBookmarksVersion", 1); // firefox 3.5.2
 user_pref("browser.places.smartBookmarksVersion", 2);
 user_pref("browser.rights.3.shown", true);
 user_pref("browser.startup.homepage", "about:blank");
-user_pref("browser.startup.homepage_override.mstone", "rv:1.9.1pre");
+user_pref("browser.startup.homepage_override.mstone", "rv:1.9.1.2");
 user_pref("browser.startup.page", 0);
-user_pref("extensions.enabledItems", "{972ce4c6-7e08-4474-a285-3208198ce6fd}:3.5pre");
-user_pref("extensions.lastAppVersion", "3.5pre");
+user_pref("extensions.enabledItems", "{972ce4c6-7e08-4474-a285-3208198ce6fd}:3.5.2");
+user_pref("extensions.lastAppVersion", "3.5.2");
 user_pref("extensions.update.notifyUser", false);
 user_pref("intl.charsetmenu.browser.cache", "ISO-8859-1, UTF-8");
 user_pref("network.cookie.prefsMigrated", true);
+//user_pref("privacy.sanitize.migrateFx3Prefs", true);
 user_pref("spellchecker.dictionary", "en-US");
 user_pref("urlclassifier.keyupdatetime.https://sb-ssl.google.com/safebrowsing/newkey", 1015584269);
+
+// mozilla engine optimizations
+user_pref("content.notify.backoffcount", 5);
+user_pref("network.dns.disableIPv6", true);
+user_pref("network.http.pipelining", true);
+user_pref("network.http.pipelining.maxrequests", 8);
+user_pref("network.http.proxy.pipelining", true);
+user_pref("network.prefetch-next", false);
+user_pref("nglayout.initialpaint.delay", 0);
+user_pref("plugin.expose_full_path", true);
+user_pref("ui.submenu.delay", 0);
+user_pref("browser.cache.memory.capacity", 16384);
