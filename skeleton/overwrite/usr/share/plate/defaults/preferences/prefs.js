4aae9a9a977f3f3138bec516ed87874974936d5e
joshas <joshas@gmail.com>
2009-10-25 14:03:49 +0000

    Removed duplicate remote-opt-get entry

--- skeleton/overwrite/usr/share/plate/defaults/preferences/prefs.js
+++ skeleton/overwrite/usr/share/plate/defaults/preferences/prefs.js
@@ -1,3 +1,1 @@
 pref("toolkit.defaultChromeURI", "chrome://plate/content/main.xul");
-pref("network.protocol-handler.app.opt-get", "/usr/local/bin/remote-opt-get");
-pref("network.protocol-handler.warn-external.opt-get", true);
