33ea45c4f5c135580105ffb378cebfb508d3e712
Chen, Ping-Hsun <penkia@gmail.com>
2009-11-03 14:30:34 +0800

    adjust post-boot.sh add opt loading function in load_data apply suggestions from kionez <kionez@gmail.com>

--- skeleton/overwrite/usr/local/bin/post-boot.sh
+++ skeleton/overwrite/usr/local/bin/post-boot.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# for aufs to merge opt-get plugin
-mount -t tmpfs none /opt
-
-# generate menu
-/usr/local/bin/update-menus
-
 /usr/bin/xmodmap ~/.Xmodmap
 
 # set mouse acceleration and threshold (pointer is too fast with new Xvesa build)
@@ -13,60 +7,69 @@ mount -t tmpfs none /opt
 
 if [ ! -e /tmp/firsttime ]; then
 
-# run this script once
-touch /tmp/firsttime
+	# run this script once
+	touch /tmp/firsttime
 
-# start udev daemon
-udevd --daemon
-udevadm trigger
+	# for aufs to merge opt-get plugin
+	mount -t tmpfs none /opt
 
-# work through NIC and wake them up
-for NIC in eth0 eth1 eth2 wlan0 ath0 ra0; do
-	/bin/ifconfig $NIC up
-	/sbin/iwconfig $NIC mode Managed
-done
+	# generate menu
+	/usr/local/bin/update-menus
 
-# try to setup DHCP for eth0
-/usr/local/bin/netdaemon eth0
+	# start udev daemon
+	udevd --daemon
+	udevadm trigger
 
-# get wireless SSID list
-/usr/local/bin/get_ssid &
+	# work through NIC and wake them up
+	/bin/ifconfig lo 127.0.0.1 netmask 255.0.0.0
 
-# setup sound channel
-CARD=`grep '0 \[' /proc/asound/cards | cut -d'[' -f2| cut -d']' -f1`
-if [ ! -z $CARD ]; then 
-sed -e "s/Intel/$CARD/g" /etc/asound.conf > /etc/asound.conf.tmp
-mv /etc/asound.conf.tmp /etc/asound.conf
-for channel in Master Front PCM; do
-	/usr/bin/amixer set $channel 95% on
-done
-fi
+	for NIC in eth0 eth1 eth2 wlan0 ath0 ra0; do
+		/bin/ifconfig $NIC up
+		/sbin/iwconfig $NIC mode Managed
+	done
 
-# post hook
-find /etc/post-boot.d/ -type f -exec {} \;  
+	# try to setup DHCP
+	/sbin/dhclient &
 
-# Atom-related setting 
-if [ "$(cat /proc/cpuinfo | grep Atom)" ] ; then
-    modprobe acpi_cpufreq 
-    modprobe cpufreq_ondemand
-    echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor 
-    echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-fi
+	# get wireless SSID list
+	/usr/local/bin/get_ssid &
 
-# mount all partitions
-for i in `fdisk -l | grep "^/dev" | cut -d' ' -f1`; do
-   mkdir -p /mnt/`basename $i`;
-   mount $i /mnt/`basename $i`;
-done
+	# mount all partitions
+	for i in `fdisk -l | grep "^/dev" | cut -d' ' -f1`; do
+	   mkdir -p /mnt/`basename $i`;
+	   mount $i /mnt/`basename $i`;
+	done
 
-# auto load user data if exist
-if [ ! "$(cat /proc/cmdline | grep xpud-data )" ]; then
-	/usr/local/bin/load_data
-fi
-# auto reconnect
-/usr/local/bin/auto-reconnect.pl &
+	# auto load user data if exist
+	if [ ! "$(cat /proc/cmdline | grep xpud-data )" ]; then
+		/usr/local/bin/load_data
+	fi
 
-# start hotplug script
-/bin/cp /sbin/hotplug-x /sbin/hotplug
+	# auto reconnect
+	/usr/local/bin/auto-reconnect.pl &
+
+	# start hotplug script
+	/bin/cp /sbin/hotplug-x /sbin/hotplug
+
+	# setup sound channel
+	CARD=`grep '0 \[' /proc/asound/cards | cut -d'[' -f2| cut -d']' -f1`
+	if [ ! -z $CARD ]; then 
+	sed -e "s/Intel/$CARD/g" /etc/asound.conf > /etc/asound.conf.tmp
+	mv /etc/asound.conf.tmp /etc/asound.conf
+	for channel in Master Front PCM; do
+		/usr/bin/amixer set $channel 95% on
+	done
+	fi
+
+	# Atom-related setting 
+	if [ "$(cat /proc/cpuinfo | grep Atom)" ] ; then
+		modprobe acpi_cpufreq 
+		modprobe cpufreq_ondemand
+		echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor 
+		echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+	fi
+
+	# post hook
+	find /etc/post-boot.d/ -type f -exec {} \;  
 
 fi 
