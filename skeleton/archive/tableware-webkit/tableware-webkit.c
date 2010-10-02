5257c9f79007a71091f2da9247062484f72e188c
Penk Chen <penkia@gmail.com>
2010-10-02 16:46:41 +0800

    implement download in tableware-webkit
    add "mime-type-policy-decision-requested"
    and "download-requested" callback functions

--- skeleton/archive/tableware-webkit/tableware-webkit.c
+++ skeleton/archive/tableware-webkit/tableware-webkit.c
@@ -2,6 +2,33 @@
 #include <stdlib.h>
 #include <webkit/webkit.h>
 
+gboolean 
+decidedownload(WebKitWebView *v, WebKitWebFrame *f, WebKitNetworkRequest *r, gchar *m,  WebKitWebPolicyDecision *p) {
+	if(!webkit_web_view_can_show_mime_type(v, m)) {
+		webkit_web_policy_decision_download(p);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+gboolean 
+initdownload(WebKitWebView *view, WebKitDownload *o) {
+	const char *filename;
+	char *uri, *html;
+
+	filename = webkit_download_get_suggested_filename(o);
+	if(!strcmp("", filename))
+		filename = "index.html";
+	uri = g_strconcat("file:///tmp/", filename, NULL);
+	webkit_download_set_destination_uri(o, uri);
+	g_free(uri);
+
+	webkit_download_get_uri(o);
+	webkit_download_start(o);
+	
+	return TRUE;
+}
+
 int main (int argc, char *argv[])
 {
 	GtkWidget *box;
@@ -24,7 +51,10 @@ int main (int argc, char *argv[])
 	gtk_container_add (GTK_CONTAINER (scrolled_window), web_view);
 
 	gtk_widget_show_all (window);
-	gtk_window_fullscreen(GTK_WINDOW (window));
+	//gtk_window_fullscreen(GTK_WINDOW (window));
+
+	g_signal_connect(G_OBJECT(WEBKIT_WEB_VIEW (web_view)), "mime-type-policy-decision-requested", G_CALLBACK(decidedownload), NULL);
+	g_signal_connect(G_OBJECT(WEBKIT_WEB_VIEW (web_view)), "download-requested", G_CALLBACK(initdownload), NULL);
 
 	gchar* uri = (gchar*) (argc > 1 ? argv[1] : "http://localhost/usr/share/plate/chrome/content");
 	webkit_web_view_open (WEBKIT_WEB_VIEW (web_view), uri);
